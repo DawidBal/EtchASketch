7846bbd1ed22fa873987be329eea8ec2768bca08
Dave <shatteredlan@gmail.com>
2021-01-10 15:06:36 +0100

    Added mouseover colorization
    Added event for every grid that changes bg color

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const divContainer = document.querySelector('.container');
-
 function createGrid(gridNumber) {
     for (let i = 1; i <= gridNumber; i++) {
         for (let j = 0; j < gridNumber; j++) {
             const divCreation = document.createElement('div');
             divCreation.classList.add('grid');
-            divCreation.textContent = i;
             divContainer.appendChild(divCreation);
         }
     }
+    // TODO: Change how to modify css styles dynamicilly
     divContainer.style.cssText = `grid-template-columns: repeat(${gridNumber}, 32px);
                                   grid-template-rows: repeat(${gridNumber}, 32px);`;
 }
 
-createGrid(16);
+function changeColor(e) {
+    e.target.classList.add('colored');
+}
+
+const divContainer = document.querySelector('.container');
+
+createGrid(32);
+
+const gridItems = Array.from(document.querySelectorAll('.grid'));
+gridItems.forEach(cell => cell.addEventListener('mouseover', changeColor));
+
