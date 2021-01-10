61fecdd0ffab9de6b9288f8edad6cb0699b0ba62
Dave <shatteredlan@gmail.com>
2021-01-10 16:09:51 +0100

    Added static grid generation

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ function createGrid(gridNumber) {
         for (let j = 0; j < gridNumber; j++) {
             const divCreation = document.createElement('div');
             divCreation.classList.add('grid');
+            divCreation.classList.add('grid-lines');
             divContainer.appendChild(divCreation);
         }
     }
     // TODO: Change how to modify css styles dynamicilly
-    divContainer.style.cssText = `grid-template-columns: repeat(${gridNumber}, 32px);
-                                  grid-template-rows: repeat(${gridNumber}, 32px);`;
+    divContainer.style.cssText = `grid-template-columns: repeat(${gridNumber}, 1fr);
+                                  grid-template-rows: repeat(${gridNumber}, 1fr);`;
 }
 
 function changeColor(e) {
@@ -16,9 +17,17 @@ function changeColor(e) {
 }
 
 const divContainer = document.querySelector('.container');
-
 createGrid(32);
-
+const btnToggleLines = document.querySelector('.options__togglelines');
 const gridItems = Array.from(document.querySelectorAll('.grid'));
+
 gridItems.forEach(cell => cell.addEventListener('mouseover', changeColor));
+
+btnToggleLines.addEventListener('click', (e) => {
+    gridItems.forEach(cell => cell.classList.toggle('grid-lines'));
+})
+
+
+
+
 
